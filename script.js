daf8244cba5d1d30e19fd246641f19cdb832b833
Vincent Sigal <vincent.sigal.pro@gmail.com>
2026-05-20 11:48:58 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const user = "vincent.sigal.pro";
+const domain = "gmail.com";
+const email = user + "@" + domain;
+
+document.getElementById("emailLink1").href = "mailto:" + email;
+document.getElementById("emailLink2").href = "mailto:" + email;
